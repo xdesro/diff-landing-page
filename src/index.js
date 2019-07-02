5df5fbb8bb4d6efc0d7f166629e19906679dc7d0
Henry Desroches <yo@henry.codes>
2019-07-01 21:03:04 -0600

    ⏰  - Add countdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import './style.scss';
+import "./style.scss";
+import * as pkg from "../package.json";
+
+const nextStreamDate = new Date(pkg.nextStream).getTime();
+
+const setTime = () => {
+  const now = new Date().getTime();
+
+  const countdown = nextStreamDate - now;
+  const timeRemaining = {
+    days: Math.floor(countdown / (1000 * 60 * 60 * 24)),
+    hours: Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    minutes: Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60))
+  };
+  Object.keys(timeRemaining).forEach(key => {
+    document.querySelector(`.countdown__${key}`).innerHTML = timeRemaining[key];
+  });
+};
+setTime();
+setInterval(setTime, 60 * 1000);
